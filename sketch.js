121bc8ef3a0dbe7506d1f984636a5dba89da809d
nehavinod7d <88573323+nehavinod7d@users.noreply.github.com>
2021-09-05 12:21:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var ball;
+var vx = 2;
+
+function setup() {
+  createCanvas(400,400);
+
+  ball = createSprite(100,100,10,10);
+  ball.shapeColor = "pink";
+}
+
+function draw() 
+{
+  background(51);
+  if(ball.position.x<=0 || ball.position.x>=width)
+  {
+    vx= -vx;
+  }
+ 
+  ball.velocity.x = vx;
+
+  drawSprites();
+
+}
+
